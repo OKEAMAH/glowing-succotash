1f279d9c99da8f96b5b625dc92c40de083da405c
OKEAMAH <whitneykiss741@gmail.com>
2024-07-02 17:54:13 +0100

    Initialize snyk-upgrade-76818317dfc2c9f100053195f6aafd76

---
+++ common/renderPNG.tsx
@@ -0,0 +1,25 @@
+import * as resvg from '@resvg/resvg-wasm'
+// @ts-ignore
+import resvgWasm from '../public/resvg_bg.wasm?module'
+
+import renderCardSVG from './renderSVG'
+import QueryType from './types/queryType'
+
+const initResvgWasm = resvg.initWasm(resvgWasm)
+
+const renderCardPNG = async (query: QueryType) => {
+  const [svg] = await Promise.all([renderCardSVG(query), initResvgWasm])
+
+  const resvgJS = new resvg.Resvg(svg, {
+    fitTo: {
+      mode: 'width',
+      value: 1280,
+    },
+  })
+  const pngData = resvgJS.render()
+  const pngBuffer = pngData.asPng()
+
+  return pngBuffer
+}
+
+export default renderCardPNG
